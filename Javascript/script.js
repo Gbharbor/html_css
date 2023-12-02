b1f51c3d2fb5a2d6867eb9023e41da60dca24f14
Drportox <guiportopt@outlook.com>
2023-12-02 11:19:08 +0000

    grid complete/first classes JS

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+let nome = ("Drportox"); let idade = 90;
+let logado = false; let lista = ["ovo","massa","corante"];
+let nomeCompleto = {nome:"Guilherme", idade:90}
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof logado);
+console.log(typeof lista);
+console.log(typeof nomeCompleto);
+ 
