0bd9a6dc59b1126c5e47edd2265a204315c19d5f
Drportox <guiportopt@outlook.com>
2023-12-15 01:14:12 +0000

    new update

--- Javascript/script.js
+++
@@ -1,9 +0,0 @@
-let nome = ("Drportox"); let idade = 90;
-let logado = false; let lista = ["ovo","massa","corante"];
-let nomeCompleto = {nome:"Guilherme", idade:90}
-console.log(typeof nome);
-console.log(typeof idade);
-console.log(typeof logado);
-console.log(typeof lista);
-console.log(typeof nomeCompleto);
- 
