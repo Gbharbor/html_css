3bc6f12714365cca3090b0871fd5368c9ef208e3
Drportox <guiportopt@outlook.com>
2024-12-23 22:33:59 +0000

    layouts & responsividade

--- TW/tailwind.config.js
+++ TW/tailwind.config.js
@@ -5,8 +5,24 @@ module.exports = {
     './P2/src/**/*.{js,ts,jsx,tsc}'// Busca por classes em arquivos JS/TS na pasta src
   ],
   theme: {
+    //container: {
+      //center: true,
+    //},
     extend: { 
-     }
+      color: {
+        'vermelho': '#ff0000',
+        'vermelhoescuro': '#990000'
+      },
+      Animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': {transform:'rotate(-3deg)'},
+          '50%':{transform:'rotate(3deg)'},
+        },
+      },
+     },
   },
   plugins: [require('@tailwindcss/line-clamp')],
 }
